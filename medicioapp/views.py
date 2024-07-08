19712dfc7b853bb79cf87b2d5118d82349e1a3be
rick170 <patrickkamau170@gmail.com>
2024-07-08 11:08:29 +0300

    initial commit

--- medicioapp/views.py
+++ medicioapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from medicioapp.models import Contact
 
 # Create your views here.
 def index(request):
@@ -6,4 +7,32 @@ def index(request):
 
 def innerpage(request):
     return render(request,'inner-page.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def doctors(request):
+    return render(request,'doctors.html')
+
+def departments(request):
+    return render(request,'departments.html')
+
+def services(request):
+    return render(request,'services.html')
+
+def contacts(request):
+    if request.method == "POST":
+        all=Contact(name=request.POST['name'],
+                    email=request.POST['email'],
+                    phone=request.POST['phone'],
+                    message=request.POST['message']
+                    )
+
+        all.save()
+        return redirect('/contacts')
+
+    else:
+        return render(request,'contact.html')
+
+
 
