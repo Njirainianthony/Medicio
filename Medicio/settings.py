2c7517d9b4bc62b34d7ade33cc6468f4ecd535a7
rick170 <patrickkamau170@gmail.com>
2024-07-22 10:44:01 +0300

    commit

--- Medicio/settings.py
+++ Medicio/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'medicioapp'
+    'medicioapp',
+    'django_daraja'
 ]
 
 MIDDLEWARE = [
